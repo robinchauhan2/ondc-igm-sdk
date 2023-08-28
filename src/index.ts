118f3efaf8fe6fa03ed69179d1a021be7b182fa7
Robin Chauhan <robinchauhan@WILASSETA0158.local>
2023-08-28 13:55:50 +0530

    fix: export interface and issue instance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import router from './igmManager/routes/igm.routes';
+import issue from './igmManager/Issue';
+import { RouteSpecificManagerProps } from './igmManager/interfaces/manager.type';
 
-export { router as issueRoutes };
+export { RouteSpecificManagerProps, issue };
